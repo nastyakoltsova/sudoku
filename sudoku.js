10aa3eab85bc0779de9fa415eb0f88baea3e0f3c
nastyakoltsova <nastyakoltsova360@gmail.com>
2022-11-05 16:13:35 +0300

    fixed: prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -4,80 +4,83 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
-  let puzzle = [];
-  let arr = boardString.split("");
-  for (let i = 0; i < arr.length; i += 9) {
-    puzzle.push(arr.slice(i, i + 9));
-  }
+    let puzzle = [];  // массив, в который мы передадим таблицу судоку
+    let str = boardString.split(""); // строка, разделенная на символы
+    for (let i = 0; i < str.length; i += 9) { // формируем таблицу
+        puzzle.push(str.slice(i, i + 9));
+    }
 
-  const coord = (puzzle) => {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (puzzle[i][j] === "-") {
-          return [i, j];
+    // функция поиска первой пустой ячейки
+    const findEmpty = (puzzle) => {
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (puzzle[i][j] === "-") {
+                    return [i, j];
+                }
+            }
         }
-      }
-    }
-    return null;
-  };
+        return null; // возвращаем null если все ячейки заполнены
+    };
 
-  const check = (number, position, puzzle) => {
-    const [i, j] = position;
+    // функция проверки повторных номеров
+    const check = (number, position, puzzle) => {
+        const [i, j] = position;
 
-    //stolbec
-    for (let x = 0; x < 9; x++) {
-      if (puzzle[x][j] === number && x !== i) {
-        return false;
-      }
-    }
-
-    //stroka
-    for (let x = 0; x < 9; x++) {
-      if (puzzle[i][x] === number && x !== j) {
-        return false;
-      }
-    }
-
-    //kvadrat
-    const boxX = Math.floor(i / 3) * 3;
-    const boxY = Math.floor(j / 3) * 3;
-
-    for (let x = boxX; x < boxX + 3; x++) {
-      for (let y = boxY; y < boxY + 3; y++) {
-        if (puzzle[x][y] === number && x !== i && y !== j) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  };
-
-  const decision = () => {
-    const position = coord(puzzle);
-    if (position === null) {
-      return true;
-    }
-    for (let i = 1; i <= 9; i++) {
-      const number = i.toString();
-      const validator = check(number, position, puzzle);
-
-      if (validator) {
-        const [x, y] = position;
-        puzzle[x][y] = number;
-
-        if (decision()) {
-          return true;
+        // в столбце
+        for (let x = 0; x < 9; x++) {
+            if (puzzle[x][j] === number) { // && x !== i
+                return false; // если есть такая же цифра в столбце
+            }
         }
 
-        puzzle[x][y] = "-";
-      }
-    }
-    return false;
-  };
+        // в строке
+        for (let x = 0; x < 9; x++) {
+            if (puzzle[i][x] === number) { // && x !== j
+                return false; // если есть такая же цифра в строке
+            }
+        }
 
-  decision();
-  return puzzle;
+        // в квадрате 3x3
+        const boxX = Math.floor(i / 3) * 3;
+        const boxY = Math.floor(j / 3) * 3;
+
+        for (let x = boxX; x < boxX + 3; x++) {
+            for (let y = boxY; y < boxY + 3; y++) {
+                if (puzzle[x][y] === number) { // && x !== i && y !== j
+                    return false; // если есть такая же цифра в квадрате
+                }
+            }
+        }
+
+        return true; // если повторов нет
+    };
+
+    // функция, которая вставляет цифры
+    const decision = () => {
+        const position = findEmpty(puzzle);
+        if (position === null) {
+            return true; // если все поле заполнено
+        }
+        for (let i = 1; i <= 9; i++) {
+            const number = i.toString();
+            const validator = check(number, position, puzzle);
+
+            if (validator) {
+                const [x, y] = position;
+                puzzle[x][y] = number;
+
+                if (decision()) { // если с данной цифрой можно решить все остальное поле
+                    return true;
+                }
+
+                puzzle[x][y] = "-"; // в случае ошибки присваиваем ячейке "-" и смотри следующее число
+            }
+        }
+        return false; // если решено неправильно
+    };
+
+    decision(); // решение
+    return puzzle;
 }
 
 /**
@@ -86,14 +89,14 @@ function solve(boardString) {
  */
 
 function isSolved(board) {
-  let result = 0;
-  for (let i = 0; i < 9; i += 1) {
-    for (let j = 0; j < 9; j += 1) {
-      res += Number(board[i][j]);
+    let result = 0;
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            result += Number(board[i][j]);
+        }
     }
-  }
 
-  return result === 405;
+    return result === 405;
 }
 
 /**
@@ -102,12 +105,32 @@ function isSolved(board) {
  * Подумай, как симпатичнее сформировать эту строку.
  */
 function prettyBoard(board) {
-  console.table(board);
+    let xx = [];
+    for (let j = 0; j < board.length; j++) {
+        if (j % 3 === 0) {
+            xx.push('\n');
+        }
+        for (let i = 0; i < board[j].length; i++) {
+            if (i % 3 === 2) {
+                if (i % 9 === 8) {
+                    let res = board[j][i] + '\n';
+                    xx.push(res);
+                } else {
+                    let res = board[j][i] + '    ';
+                    xx.push(res);
+                }
+            } else {
+                let res = board[j][i] + '  ';
+                xx.push(res);
+            }
+        }
+    }
+console.log(xx.join(''))
 }
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
+    solve,
+    isSolved,
+    prettyBoard,
 };
